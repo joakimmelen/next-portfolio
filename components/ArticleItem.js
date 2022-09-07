33ffe13d9b0161e844bed768b52ecba9d831b7b0
SMOKINFRY <jcrooge@gmail.com>
2022-09-07 11:28:23 +0200

    seocnd init commiting

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import Link from "next/link"
+import articleStyles from "../styles/Article.module.css"
 
-const ArticleItem = () => {
+const ArticleItem = ({article}) => {
   return (
-    <div>ArticleItem</div>
+    <Link href="/article/[id]" as={`/article/${article.id}`}>
+        <a className={articleStyles.card}>
+            <h3>{article.title} &rarr;</h3>
+            <p>{article.body}</p>
+        </a>
+    </Link>
   )
 }
 
--- components/ArticleList.js
+++ components/ArticleList.js
@@ -1,10 +1,11 @@
+import ArticleItem from './ArticleItem'
 import articleStyles from '../styles/Article.module.css'
 
 const ArticleList = ({articles}) => {
   return (
     <div className={articleStyles.grid}>
         {articles.map((article) => (
-            <h3>{article.title}</h3>
+            <ArticleItem key={article.id} article={article} />
         ))}
     </div>
   )
